e5178198500c9f352a8c08c0bb3dae1c8ceac730
StefanoCodes <stefanovidmarbusiness@gmail.com>
2026-02-19 19:26:35 +0100

    feat: added image quality settings to next.config.ts for improved performance

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ const nextConfig: NextConfig = {
   images: {
     formats: ["image/avif", "image/webp"],
     deviceSizes: [400, 640, 750, 828, 1080, 1200],
+    qualities: [40, 50, 55, 65, 75],
     minimumCacheTTL: 31536000,
   },
   compress: true,
