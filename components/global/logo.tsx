291dcb9515b36e059f269b8ca0c7f94209abff48
StefanoCodes <stefanovidmarbusiness@gmail.com>
2026-03-15 23:29:15 +0100

    fix: update logo image source and dimensions for better quality
    Refactor logo component to use a CDN function for image sourcing, enhancing image quality. Increased dimensions to 480x113 for improved rendering on high-DPI displays.

--- components/global/logo.tsx
+++ components/global/logo.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
-import { images } from "@/lib/cdn";
+import { cdn, images } from "@/lib/cdn";
 import { cn } from "@/lib/utils";
 
 interface LogoProps extends React.HTMLAttributes<HTMLAnchorElement> {
@@ -15,10 +15,10 @@ export function Logo({ className, ...props }: LogoProps) {
       {...props}
     >
       <Image
-        src={`https://res.cloudinary.com/deoxevgc6/image/upload/${images.brand.logo}`}
+        src={cdn(images.brand.logo, { width: 960, quality: "100" })}
         alt="Firstline"
-        width={360}
-        height={84}
+        width={480}
+        height={113}
         className="h-7 w-auto"
         unoptimized
       />
